f80eceb93d04f79c0e4e20513ec4430bb61e3c88
SnoopyKim <snoopykim0@gmail.com>
2022-05-30 13:03:42 +0900

    코드 정리

--- lib/screens/second.dart
+++ lib/screens/second.dart
@@ -8,16 +8,24 @@ class SecondScreen extends StatefulWidget {
 }
 
 class _SecondScreenState extends State<SecondScreen> {
-  String _input = '';
+  final formKey = GlobalKey<FormState>();
 
-  goNext() {
-    // 다른 페이지로 인수 값을 넘길땐 여러 방법이 있지만 argument에 담아보내는게 일반적
-    // 블로그 같은 형식일 경우 route 자체에 parameter를 더해서 보내는 경우가 대부분
-    // + 우리의 경우 다음 페이지에서 반드시 필요로 하는 정보가 있으므로 (번호)
-    // + 여기서 입력값에 대한 validate를 해주거나,
-    // + 다음 페이지에서 에러 페이지를 고려해야함.
-    // + 이번 케이스에선 둘다 필요할 듯 (여기선 번호 입력에 대한 validate, 다음 페이지에선 DB에 있는지 검사)
-    Navigator.pushNamed(context, '/second/present');
+  goNext(String input) {
+    if (formKey.currentState?.validate() ?? false) {
+      Navigator.pushNamed(context, '/second/present', arguments: input);
+    }
+  }
+
+  String? phoneValidator(String? phone) {
+    if (phone == null || phone.replaceAll(' ', '').isEmpty) {
+      return '번호를 입력해주세요';
+    }
+
+    if (!RegExp(r'^[0-9](\d{10,10})').hasMatch(phone)) {
+      return '숫자로 11자리를 입력해주세요';
+    }
+
+    return null;
   }
 
   @override
@@ -30,53 +38,40 @@ class _SecondScreenState extends State<SecondScreen> {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            logo(), //로고
-            Text(
+            Image.asset('assets/images/logo.png', width: 217),
+            const Text(
               '당신의 파트너는 누구일까요?',
               textScaleFactor: 1.0,
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
             ),
             const SizedBox(height: 30.0),
-            TextField(
-              decoration: InputDecoration(
-                  labelText: '전화번호(\'-\' 없이 입력하세요)',
-                  hintText: '01012345678',
-                  hintStyle: TextStyle(
-                    height: 2.0,
-                  ),
-                  contentPadding: EdgeInsets.all(10.0)),
-              onChanged: (value) => setState(() {
-                _input = value;
-              }),
-              onSubmitted: (_) => goNext(),
+            Form(
+              key: formKey,
+              child: TextFormField(
+                keyboardType: TextInputType.phone,
+                decoration: const InputDecoration(
+                    labelText: "전화번호('-' 없이 입력하세요)",
+                    hintText: '01012345678',
+                    contentPadding:
+                        EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0)),
+                validator: phoneValidator,
+                onFieldSubmitted: (input) => goNext(input),
+                onSaved: (input) => goNext(input ?? ''),
+              ),
             ),
             const SizedBox(height: 30.0),
             TextButton(
-              // 호옹 styleFrom으로 커스텀한거 개 굳
-              // 만약 완전 자유롭게? 만들고 싶다면 GestureDetector가 있음
               style: TextButton.styleFrom(
                   padding: const EdgeInsets.all(20.0),
                   textStyle: const TextStyle(fontSize: 20),
                   primary: Colors.white,
-                  backgroundColor: Color(0xFF172E63)),
-              // 인자가 같을 경우는 Arrow Function 말고 그냥
-              onPressed: () => goNext(),
-              child: Text('NEXT'),
+                  backgroundColor: const Color(0xFF172E63)),
+              onPressed: () => formKey.currentState?.save(),
+              child: const Text('NEXT'),
             )
           ],
         ),
       ),
     ));
-  }
-
-  // state에 따른 변경되는 위젯이 아니거나,
-  // 가독성을 높이기 위해서가 아니라면 함수로 빼서 렌더링하는 거는 굳이 불필요
-  // 위젯 클래스나 직접 박으면 해당 위젯이 재활용?이 되는 반면에
-  // 함수로 렌더링할 경우 똑같은 위젯이여도 아예 삭제 후 재생성을 하기에 성능 상 이슈가 있음
-  // (사실 큰 차이 없음 state에 따라 변하는 게 아닌 이상은 거의 고려안해도 될 정도)
-  Widget logo() {
-    return Image.asset('assets/images/logo.png', width: 217);
-    // 참고로 Image에서 width는 보통 이미지를 담고 있는 박스? 의 크기를 뜻함
-    // 이미지 자체는 그 안에 BoxFit 설정에 따라 렌더링되는거 (자동 resize?)
   }
 }
--- lib/screens/second_present.dart
+++ lib/screens/second_present.dart
@@ -1,169 +1,191 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
 
-class SecondPresentScreen extends StatelessWidget {
+class SecondPresentScreen extends StatefulWidget {
   const SecondPresentScreen({Key? key}) : super(key: key);
 
-  Widget logo() {
-    return Image.asset('assets/images/logo.png', width: 100);
+  @override
+  State<SecondPresentScreen> createState() => _SecondPresentScreenState();
+}
+
+class _SecondPresentScreenState extends State<SecondPresentScreen> {
+  Future getDataFromPhone(String phone) async {
+    await Future.delayed(Duration(milliseconds: 500));
+    return phone;
   }
 
   Widget presentCode() {
-    // Column에도 mainAxisAlignment에 center하면 가운데임
-    // 이 경우엔 굳이 min에다가 center로 감싸주지 않아도 ㄱㅊ
-    // 인데 사실 방법의 차이일 뿐 뭐가 더 좋은게 아님
-    // 가운데 배치하는 방법이 많아서....
     return Container(
         height: 150,
         width: 150,
-        alignment: Alignment.center,
-        // 오 Decoration으로 자주 쓰는건 한방에 다 써버렸넼ㅋㅋㅋㅋㅋ 굳
-        // 참고로 알겠지만 borderWidth는 width에 포함안되므로 이 위젯의 width/height는 158임
+        margin: const EdgeInsets.symmetric(vertical: 30.0),
         decoration: BoxDecoration(
             color: Colors.white,
-            border: Border.all(color: Color(0xFF172E63), width: 4),
+            border: Border.all(color: const Color(0xFF172E63), width: 4),
             shape: BoxShape.circle,
             boxShadow: [
               BoxShadow(
-                  color: Color(0xFF172E63).withOpacity(0.4),
+                  color: const Color(0xFF172E63).withOpacity(0.4),
                   spreadRadius: 2,
                   blurRadius: 6,
-                  offset: Offset(3, 3))
+                  offset: const Offset(3, 3))
             ]),
         child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              // textScaleFactor는 보통 기본값이 1.0
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const [
               Text(
                 '경품 추첨 코드',
-                textScaleFactor: 1.0,
                 textAlign: TextAlign.center,
                 style: TextStyle(
-                    height: 1.2,
                     fontWeight: FontWeight.bold,
                     fontSize: 13,
-                    color: Color(0xFF172E63)
-                    // color: Colors.white
-                    ),
+                    color: Color(0xFF172E63)),
               ),
               Text(
                 '54',
-                textScaleFactor: 1.0,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                     height: 1.2,
                     fontWeight: FontWeight.bold,
                     fontSize: 55,
-                    color: Color(0xFF172E63)
-                    // color: Colors.white
-                    ),
+                    color: Color(0xFF172E63)),
               ),
             ]));
   }
 
-  // 옼ㅋㅋㅋㅋㅋㅋ 중복제거용 위젯 함수 굳
   Widget title(String title, String desc) {
-    // 알다시피 Container에도 alignment 있슴돠
-    return Align(
+    return Container(
         alignment: Alignment.topLeft,
-        child: Container(
-            padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Text(title,
-                  textScaleFactor: 1.0,
-                  style: TextStyle(
-                      height: 1.2,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Color(0xFF172E63))),
-              Text(desc,
-                  textScaleFactor: 1.0,
-                  style: TextStyle(
-                    height: 1.2,
-                    fontWeight: FontWeight.normal,
-                    fontSize: 13,
-                  ))
-            ])));
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+          Text(title,
+              style: const TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Color(0xFF172E63))),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 12.0),
+            child: Text(desc,
+                style: const TextStyle(
+                  height: 1.2,
+                  fontSize: 13,
+                )),
+          )
+        ]));
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // Column이 min이 아니면 가질 수 있는 최대의 높이로 가져감
-      // => 해당 Column의 crossAxisAlignment 기본값이 center 이므로, Center로 감싸줄 필요가 없음
-      body: Center(
+  Widget _buildResultPage(Object? data) {
+    return Container(
+      height: double.infinity,
+      constraints: const BoxConstraints(maxWidth: 500),
+      child: SingleChildScrollView(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
         child: Column(children: [
-          const SizedBox(height: 20.0),
-          logo(),
-          const SizedBox(height: 30.0),
           presentCode(),
-          const SizedBox(height: 30.0),
           title('KEYWORD', '당신과 같은 키워드를 가진 사람을 찾아, 선물을 교환하세요!'),
-          // 보통 shadow까지 줬다면 horizontal 여백은 최소 20 (앱 기준)
           Container(
-              width: double.infinity,
               height: 100,
-              margin: EdgeInsets.all(10),
+              alignment: Alignment.center,
               decoration: BoxDecoration(
-                  color: Color(0xFF172E63),
+                  color: const Color(0xFF172E63),
                   boxShadow: [
                     BoxShadow(
-                        color: Color(0xFF172E63).withOpacity(0.4),
+                        color: const Color(0xFF172E63).withOpacity(0.4),
                         spreadRadius: 2,
                         blurRadius: 6,
-                        offset: Offset(3, 3))
+                        offset: const Offset(3, 3))
                   ],
                   borderRadius: BorderRadius.circular(10)),
-              child: Align(
-                alignment: Alignment.center,
-                child: Text(
-                  '혹시 당근?',
-                  textScaleFactor: 1.0,
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.white),
-                ),
+              child: const Text(
+                '혹시 당근?',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                    color: Colors.white),
               )),
-          const SizedBox(height: 10),
+          const SizedBox(height: 20),
           title('HINT', '파트너를 알아볼 수 있는 특징은?'),
           Container(
-              width: double.infinity,
               height: 60,
-              margin: EdgeInsets.all(10),
+              alignment: Alignment.center,
               decoration: BoxDecoration(
                   color: Colors.white,
                   boxShadow: [
                     BoxShadow(
-                        color: Color(0xFF172E63).withOpacity(0.4),
+                        color: const Color(0xFF172E63).withOpacity(0.4),
                         spreadRadius: 2,
                         blurRadius: 6,
-                        offset: Offset(3, 3))
+                        offset: const Offset(3, 3))
                   ],
-                  border: Border.all(color: Color(0xFF172E63), width: 2),
+                  border: Border.all(color: const Color(0xFF172E63), width: 2),
                   borderRadius: BorderRadius.circular(10)),
-              child: Align(
-                alignment: Alignment.center,
-                child: Text(
-                  '술톤 개구리',
-                  textScaleFactor: 1.0,
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 15,
-                      color: Color(0xFF172E63)),
-                ),
-              ))
+              child: const Text(
+                '술톤 개구리',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                    color: Color(0xFF172E63)),
+              )),
         ]),
       ),
     );
   }
-}
 
-// 대체로 잘못한 부분은 전혀 없고
-// 사소한 거나 굳이 안해도 되는 부분 같은거 밖에 없네 
-// 처음하는걸텐데 아주 굳임. 검색 좀 해본 티가 난다.
+  Widget _buildErrorPage() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(
+          Icons.error_outline,
+          size: 100,
+          color: Theme.of(context).colorScheme.primary,
+        ),
+        const Padding(
+          padding: EdgeInsets.symmetric(vertical: 20.0),
+          child: Text('해당 번호로 등록된 멤버가 없습니다'),
+        ),
+        ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              primary: Theme.of(context).primaryColor,
+              onPrimary: Theme.of(context).colorScheme.onPrimary,
+            ),
+            onPressed: () => Navigator.pop(context),
+            child: const Text('뒤로가기')),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String phone = (ModalRoute.of(context)?.settings.arguments ?? '') as String;
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        toolbarHeight: 90,
+        title: Image.asset(
+          'assets/images/logo.png',
+          height: 50,
+        ),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: phone.isNotEmpty
+            ? FutureBuilder(
+                future: getDataFromPhone(phone),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const CircularProgressIndicator();
+                  }
+
+                  if (snapshot.hasError || false) {
+                    return _buildErrorPage();
+                  }
+
+                  return _buildResultPage(snapshot.data);
+                })
+            : _buildErrorPage(),
+      ),
+    );
+  }
+}
