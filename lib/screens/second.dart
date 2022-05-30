fd5b953dd06e79850e1c292a37319fa70d9a8a34
SnoopyKim <snoopykim0@gmail.com>
2022-05-30 10:16:02 +0900

    로딩 색 변경 & 수진이 코드 피드백

--- lib/screens/second.dart
+++ lib/screens/second.dart
@@ -1,7 +1,3 @@
-// ignore_for_file: prefer_const_constructors
-
-import 'dart:html';
-
 import 'package:flutter/material.dart';
 
 class SecondScreen extends StatefulWidget {
@@ -15,6 +11,12 @@ class _SecondScreenState extends State<SecondScreen> {
   String _input = '';
 
   goNext() {
+    // 다른 페이지로 인수 값을 넘길땐 여러 방법이 있지만 argument에 담아보내는게 일반적
+    // 블로그 같은 형식일 경우 route 자체에 parameter를 더해서 보내는 경우가 대부분
+    // + 우리의 경우 다음 페이지에서 반드시 필요로 하는 정보가 있으므로 (번호)
+    // + 여기서 입력값에 대한 validate를 해주거나,
+    // + 다음 페이지에서 에러 페이지를 고려해야함.
+    // + 이번 케이스에선 둘다 필요할 듯 (여기선 번호 입력에 대한 validate, 다음 페이지에선 DB에 있는지 검사)
     Navigator.pushNamed(context, '/second/present');
   }
 
@@ -50,11 +52,14 @@ class _SecondScreenState extends State<SecondScreen> {
             ),
             const SizedBox(height: 30.0),
             TextButton(
+              // 호옹 styleFrom으로 커스텀한거 개 굳
+              // 만약 완전 자유롭게? 만들고 싶다면 GestureDetector가 있음
               style: TextButton.styleFrom(
                   padding: const EdgeInsets.all(20.0),
                   textStyle: const TextStyle(fontSize: 20),
                   primary: Colors.white,
                   backgroundColor: Color(0xFF172E63)),
+              // 인자가 같을 경우는 Arrow Function 말고 그냥
               onPressed: () => goNext(),
               child: Text('NEXT'),
             )
@@ -64,7 +69,14 @@ class _SecondScreenState extends State<SecondScreen> {
     ));
   }
 
+  // state에 따른 변경되는 위젯이 아니거나,
+  // 가독성을 높이기 위해서가 아니라면 함수로 빼서 렌더링하는 거는 굳이 불필요
+  // 위젯 클래스나 직접 박으면 해당 위젯이 재활용?이 되는 반면에
+  // 함수로 렌더링할 경우 똑같은 위젯이여도 아예 삭제 후 재생성을 하기에 성능 상 이슈가 있음
+  // (사실 큰 차이 없음 state에 따라 변하는 게 아닌 이상은 거의 고려안해도 될 정도)
   Widget logo() {
     return Image.asset('assets/images/logo.png', width: 217);
+    // 참고로 Image에서 width는 보통 이미지를 담고 있는 박스? 의 크기를 뜻함
+    // 이미지 자체는 그 안에 BoxFit 설정에 따라 렌더링되는거 (자동 resize?)
   }
 }
--- lib/screens/second_present.dart
+++ lib/screens/second_present.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 class SecondPresentScreen extends StatelessWidget {
@@ -10,10 +12,16 @@ class SecondPresentScreen extends StatelessWidget {
   }
 
   Widget presentCode() {
+    // Column에도 mainAxisAlignment에 center하면 가운데임
+    // 이 경우엔 굳이 min에다가 center로 감싸주지 않아도 ㄱㅊ
+    // 인데 사실 방법의 차이일 뿐 뭐가 더 좋은게 아님
+    // 가운데 배치하는 방법이 많아서....
     return Container(
         height: 150,
         width: 150,
         alignment: Alignment.center,
+        // 오 Decoration으로 자주 쓰는건 한방에 다 써버렸넼ㅋㅋㅋㅋㅋ 굳
+        // 참고로 알겠지만 borderWidth는 width에 포함안되므로 이 위젯의 width/height는 158임
         decoration: BoxDecoration(
             color: Colors.white,
             border: Border.all(color: Color(0xFF172E63), width: 4),
@@ -29,6 +37,7 @@ class SecondPresentScreen extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisSize: MainAxisSize.min,
             children: [
+              // textScaleFactor는 보통 기본값이 1.0
               Text(
                 '경품 추첨 코드',
                 textScaleFactor: 1.0,
@@ -56,7 +65,9 @@ class SecondPresentScreen extends StatelessWidget {
             ]));
   }
 
+  // 옼ㅋㅋㅋㅋㅋㅋ 중복제거용 위젯 함수 굳
   Widget title(String title, String desc) {
+    // 알다시피 Container에도 alignment 있슴돠
     return Align(
         alignment: Alignment.topLeft,
         child: Container(
@@ -83,6 +94,8 @@ class SecondPresentScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // Column이 min이 아니면 가질 수 있는 최대의 높이로 가져감
+      // => 해당 Column의 crossAxisAlignment 기본값이 center 이므로, Center로 감싸줄 필요가 없음
       body: Center(
         child: Column(children: [
           const SizedBox(height: 20.0),
@@ -91,6 +104,7 @@ class SecondPresentScreen extends StatelessWidget {
           presentCode(),
           const SizedBox(height: 30.0),
           title('KEYWORD', '당신과 같은 키워드를 가진 사람을 찾아, 선물을 교환하세요!'),
+          // 보통 shadow까지 줬다면 horizontal 여백은 최소 20 (앱 기준)
           Container(
               width: double.infinity,
               height: 100,
@@ -149,3 +163,7 @@ class SecondPresentScreen extends StatelessWidget {
     );
   }
 }
+
+// 대체로 잘못한 부분은 전혀 없고
+// 사소한 거나 굳이 안해도 되는 부분 같은거 밖에 없네 
+// 처음하는걸텐데 아주 굳임. 검색 좀 해본 티가 난다.
